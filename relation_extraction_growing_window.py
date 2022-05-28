da7d713ea3d4ae1cf0b05936deede991deab5c98
unknown <ales.kert@gmail.com>
2022-05-28 17:57:52 +0200

    mainified

--- relation_extraction_growing_window.py
+++ relation_extraction_growing_window.py
@@ -44,209 +44,209 @@ def predict_line(line) :
     return detection, max_val
 
 
-
-device = torch.device('cuda')
-
-conf = {'experiment': 'EN_reg_nonhier+def',
-        'model_id': 'allenai/scibert_scivocab_cased',
-        'max_length': 128,
-        'batch_size': 4,
-        'epochs': 5}
-conf['model_dir'] = os.path.join('data', 'experiments', conf['experiment'], model_id_to_path(conf['model_id']))
-conf['eval_dir'] = conf['model_dir']
-conf['data_dir'] = os.path.join('data', 'experiments', conf['experiment'])
-
-tokenizer = get_tokenizer(conf['model_id'])
-processor = TermFrameProcessor(conf)
-
-args = torch.load(os.path.join(conf['model_dir'], "training_args.bin"))
-model = RBERT.from_pretrained(os.path.join(conf['model_dir'], 'model.pt'), args=args)
-model.to(device);
-model.eval()
-
-
-#sentence = "Geer moraines or <e1> washboard moraines </e1> are series of small and roughly parallel ridges of till that are ordinarily associated with lakes or former lakes ."
-
-sentences = []
-test_sentences = {}
-
-with open(os.path.join(conf['data_dir'], 'test.tsv'), 'r') as fp:
-    lines = fp.readlines()
-    for line in lines :
-        sentence_raw = line.split('\t')[1]
-        sentence_proc = sentence_raw.replace('<e2> ', '')
-        sentence_proc = sentence_proc.replace('</e2> ', '')
-        if sentence_proc not in test_sentences :
-            test_sentences[sentence_proc] = []
-        test_sentences[sentence_proc].append([line.split('\t')[0], line.split('\t')[1]])
-            
-arr_test_sent = []
-
-for test_sent in test_sentences :
-    sent_group = []
+if __name__ == '__main__':
+    device = torch.device('cuda')
     
-    for sent in test_sentences[test_sent] :
-        sent_group.append(sent)
+    conf = {'experiment': 'EN_reg_nonhier+def',
+            'model_id': 'allenai/scibert_scivocab_cased',
+            'max_length': 128,
+            'batch_size': 4,
+            'epochs': 5}
+    conf['model_dir'] = os.path.join('data', 'experiments', conf['experiment'], model_id_to_path(conf['model_id']))
+    conf['eval_dir'] = conf['model_dir']
+    conf['data_dir'] = os.path.join('data', 'experiments', conf['experiment'])
     
-    arr_test_sent.append(sent_group)
-
-
-with open(os.path.join(conf['data_dir'], 'test.tsv'), 'r') as fp:
-    lines = fp.readlines()
-    for line in lines :
-        sentence_raw = line.split('\t')[1]
-        sentence_proc = sentence_raw.replace('<e2> ', '')
-        sentence_proc = sentence_proc.replace('</e2> ', '')
-        if sentence_proc not in sentences :
-            sentences.append(sentence_proc)
-        
-all_preds = []
-
-labels = get_label(conf)
-
-for sentence in tqdm(sentences) :
-    preds = []
-    i1 = sentence.find('<e1>')
-    i2 = sentence.find('</e1>')
-    window_size_start = 2
-    window_size = window_size_start
-    words_before = sentence[:i1].strip().split(' ')
-    words_after = sentence[i2:].strip().split(' ')
+    tokenizer = get_tokenizer(conf['model_id'])
+    processor = TermFrameProcessor(conf)
     
-    idx1 = 0
-    while idx1 < (len(words_before) - window_size + 1):
-        idx2 = idx1 + window_size
-        e2_before = words_before[:idx1] + ['<e2>'] + words_before[idx1:idx2] + ['</e2>'] + words_before[idx2:]
-        line = [['Other', ' '.join(e2_before) + ' ' + sentence[i1:]]]
-        prediction, confidence = predict_line(line)
-        
-        if prediction != 0 :
-            max_confidence = confidence
-            nu_prediction = prediction
-            nu_confidence = confidence
-            e2_before_nu = e2_before
-            
-            window_size += 1
-            while prediction == nu_prediction and idx2 < len(words_before) and max_confidence >= nu_confidence - 1 :
-                e2_before = e2_before_nu
+    args = torch.load(os.path.join(conf['model_dir'], "training_args.bin"))
+    model = RBERT.from_pretrained(os.path.join(conf['model_dir'], 'model.pt'), args=args)
+    model.to(device);
+    model.eval()
+    
+    
+    #sentence = "Geer moraines or <e1> washboard moraines </e1> are series of small and roughly parallel ridges of till that are ordinarily associated with lakes or former lakes ."
+    
+    sentences = []
+    test_sentences = {}
+    
+    with open(os.path.join(conf['data_dir'], 'test.tsv'), 'r') as fp:
+        lines = fp.readlines()
+        for line in lines :
+            sentence_raw = line.split('\t')[1]
+            sentence_proc = sentence_raw.replace('<e2> ', '')
+            sentence_proc = sentence_proc.replace('</e2> ', '')
+            if sentence_proc not in test_sentences :
+                test_sentences[sentence_proc] = []
+            test_sentences[sentence_proc].append([line.split('\t')[0], line.split('\t')[1]])
                 
-                idx2 = idx1 + window_size
-                e2_before_nu = words_before[:idx1] + ['<e2>'] + words_before[idx1:idx2] + ['</e2>'] + words_before[idx2:]
-                nu_line = [['Other', ' '.join(e2_before_nu) + ' ' + sentence[i1:]]]
-                nu_prediction, nu_confidence = predict_line(nu_line)
-                if nu_confidence > max_confidence :
-                    max_confidence = nu_confidence
-                
-                window_size += 1
-                
-            preds.append([labels[prediction], ' '.join(e2_before) + ' ' + sentence[i1:]])
-            idx1 += window_size - 1
-            window_size = window_size_start
-        
-        idx1 += 1
+    arr_test_sent = []
     
-    idx1 = 0
-    while idx1 < len(words_after):
-        idx2 = idx1 + window_size
-        e2_after = words_after[:idx1] + ['<e2>'] + words_after[idx1:idx2] + ['</e2>'] + words_after[idx2:]
-        line = [['Other', sentence[:i2] + ' ' + ' '.join(e2_after)]]
-        prediction, confidence = predict_line(line)
+    for test_sent in test_sentences :
+        sent_group = []
         
-        if prediction != 0 :
-            max_confidence = confidence
-            nu_prediction = prediction
-            nu_confidence = confidence
-            e2_after_nu = e2_after
+        for sent in test_sentences[test_sent] :
+            sent_group.append(sent)
+        
+        arr_test_sent.append(sent_group)
+    
+    
+    with open(os.path.join(conf['data_dir'], 'test.tsv'), 'r') as fp:
+        lines = fp.readlines()
+        for line in lines :
+            sentence_raw = line.split('\t')[1]
+            sentence_proc = sentence_raw.replace('<e2> ', '')
+            sentence_proc = sentence_proc.replace('</e2> ', '')
+            if sentence_proc not in sentences :
+                sentences.append(sentence_proc)
             
-            window_size += 1
+    all_preds = []
+    
+    labels = get_label(conf)
+    
+    for sentence in tqdm(sentences) :
+        preds = []
+        i1 = sentence.find('<e1>')
+        i2 = sentence.find('</e1>')
+        window_size_start = 2
+        window_size = window_size_start
+        words_before = sentence[:i1].strip().split(' ')
+        words_after = sentence[i2:].strip().split(' ')
+        
+        idx1 = 0
+        while idx1 < (len(words_before) - window_size + 1):
             idx2 = idx1 + window_size
-            while prediction == nu_prediction and idx2 < len(words_after) and max_confidence >= nu_confidence - 1 :
-                e2_after = e2_after_nu
+            e2_before = words_before[:idx1] + ['<e2>'] + words_before[idx1:idx2] + ['</e2>'] + words_before[idx2:]
+            line = [['Other', ' '.join(e2_before) + ' ' + sentence[i1:]]]
+            prediction, confidence = predict_line(line)
+            
+            if prediction != 0 :
+                max_confidence = confidence
+                nu_prediction = prediction
+                nu_confidence = confidence
+                e2_before_nu = e2_before
                 
-                e2_after_nu = words_after[:idx1] + ['<e2>'] + words_after[idx1:idx2] + ['</e2>'] + words_after[idx2:]
-                nu_line = [['Other', sentence[:i2] + ' ' + ' '.join(e2_after_nu)]]
-                nu_prediction, nu_confidence = predict_line(nu_line)
-                if nu_confidence > max_confidence :
-                    max_confidence = nu_confidence
+                window_size += 1
+                while prediction == nu_prediction and idx2 < len(words_before) and max_confidence >= nu_confidence - 1 :
+                    e2_before = e2_before_nu
+                    
+                    idx2 = idx1 + window_size
+                    e2_before_nu = words_before[:idx1] + ['<e2>'] + words_before[idx1:idx2] + ['</e2>'] + words_before[idx2:]
+                    nu_line = [['Other', ' '.join(e2_before_nu) + ' ' + sentence[i1:]]]
+                    nu_prediction, nu_confidence = predict_line(nu_line)
+                    if nu_confidence > max_confidence :
+                        max_confidence = nu_confidence
+                    
+                    window_size += 1
+                    
+                preds.append([labels[prediction], ' '.join(e2_before) + ' ' + sentence[i1:]])
+                idx1 += window_size - 1
+                window_size = window_size_start
+            
+            idx1 += 1
+        
+        idx1 = 0
+        while idx1 < len(words_after):
+            idx2 = idx1 + window_size
+            e2_after = words_after[:idx1] + ['<e2>'] + words_after[idx1:idx2] + ['</e2>'] + words_after[idx2:]
+            line = [['Other', sentence[:i2] + ' ' + ' '.join(e2_after)]]
+            prediction, confidence = predict_line(line)
+            
+            if prediction != 0 :
+                max_confidence = confidence
+                nu_prediction = prediction
+                nu_confidence = confidence
+                e2_after_nu = e2_after
                 
                 window_size += 1
                 idx2 = idx1 + window_size
+                while prediction == nu_prediction and idx2 < len(words_after) and max_confidence >= nu_confidence - 1 :
+                    e2_after = e2_after_nu
+                    
+                    e2_after_nu = words_after[:idx1] + ['<e2>'] + words_after[idx1:idx2] + ['</e2>'] + words_after[idx2:]
+                    nu_line = [['Other', sentence[:i2] + ' ' + ' '.join(e2_after_nu)]]
+                    nu_prediction, nu_confidence = predict_line(nu_line)
+                    if nu_confidence > max_confidence :
+                        max_confidence = nu_confidence
+                    
+                    window_size += 1
+                    idx2 = idx1 + window_size
+                    
+                preds.append([labels[prediction], sentence[:i2] + ' ' + ' '.join(e2_after)])
+                idx1 += window_size - 2
+                window_size = window_size_start
+            else :
+                idx1 += 1
                 
-            preds.append([labels[prediction], sentence[:i2] + ' ' + ' '.join(e2_after)])
-            idx1 += window_size - 2
-            window_size = window_size_start
-        else :
-            idx1 += 1
-            
-    all_preds.append(preds)
-    # lines.append(['HAS_FUNCTION', '<e1> Grab samplers </e1> are buckets or segments that <e2> drive into the sediment layer and enclose and retain a layer </e2> .'])
- 
-backup_preds = all_preds   
-
-
-all_preds = arr_test_sent
-all_preds = backup_preds
-gt = []
-prediction = []
-
-
-with open(os.path.join(conf['model_dir'], 'test_reg.csv'), 'w') as fa:
-    fa.write('Sentence,Word,Tag\n')
+        all_preds.append(preds)
+        # lines.append(['HAS_FUNCTION', '<e1> Grab samplers </e1> are buckets or segments that <e2> drive into the sediment layer and enclose and retain a layer </e2> .'])
+     
+    backup_preds = all_preds   
     
-    for sent_idx, preds in enumerate(all_preds) :
-        if len(preds) > 0:
-            tagged_sentence = ['O'] * (len(preds[0][1].replace('  ', ' ').split(' ')) - 4)
-            tokens = []
-            
-            for i, pred in enumerate(preds) :
-                is_def = False
-                is_rel = False
-                test = pred[1].replace('  ', ' ').split(' ') 
-                relation = pred[0]
-                idx = 0
-                for word in pred[1].replace('  ', ' ').split(' ') :
-                    if not is_def and '<e1>' in word :
-                        is_def = True
-                        continue
-                    elif is_def and '</e1>' in word :
-                        is_def = False
-                        continue
-                        
-                    if not is_rel and '<e2>' in word :
-                        is_rel = True
-                        continue
-                    elif is_rel and '</e2>' in word :
-                        is_rel = False
-                        continue
-                    
-                    if is_def :
-                        tagged_sentence[idx] = 'DEFINIENDUM'
-                    if is_rel :
-                        tagged_sentence[idx] = relation
-                    
-                    if i == 0 :
-                        tokens.append(word)
-                    idx += 1
-            
-            for idx, toke in enumerate(tokens) :
-                fa.write(str(sent_idx) + ',' + toke.replace('\n', '') + ',' + tagged_sentence[idx] + '\n')
-            
-            #prediction.append(tagged_sentence)
-            
-prediction.insert(8, ['O'] * len(gt[8]))
-prediction.insert(15, ['O'] * len(gt[15]))  
-
-
-from seqeval.metrics import classification_report
-
-report = classification_report(gt, prediction)
-with open(os.path.join(conf['model_dir'], 'results_reg.txt'), 'w') as fl:
-    fl.write(report)
-
-
+    
+    all_preds = arr_test_sent
+    all_preds = backup_preds
+    gt = []
+    prediction = []
+    
+    
+    with open(os.path.join(conf['model_dir'], 'test_reg.csv'), 'w') as fa:
+        fa.write('Sentence,Word,Tag\n')
         
-"""
-gt = []
-prediction =
-with open(os.path.join(conf['model_dir'], 'test_reg.csv'), 'r') as fa:
-"""
+        for sent_idx, preds in enumerate(all_preds) :
+            if len(preds) > 0:
+                tagged_sentence = ['O'] * (len(preds[0][1].replace('  ', ' ').split(' ')) - 4)
+                tokens = []
+                
+                for i, pred in enumerate(preds) :
+                    is_def = False
+                    is_rel = False
+                    test = pred[1].replace('  ', ' ').split(' ') 
+                    relation = pred[0]
+                    idx = 0
+                    for word in pred[1].replace('  ', ' ').split(' ') :
+                        if not is_def and '<e1>' in word :
+                            is_def = True
+                            continue
+                        elif is_def and '</e1>' in word :
+                            is_def = False
+                            continue
+                            
+                        if not is_rel and '<e2>' in word :
+                            is_rel = True
+                            continue
+                        elif is_rel and '</e2>' in word :
+                            is_rel = False
+                            continue
+                        
+                        if is_def :
+                            tagged_sentence[idx] = 'DEFINIENDUM'
+                        if is_rel :
+                            tagged_sentence[idx] = relation
+                        
+                        if i == 0 :
+                            tokens.append(word)
+                        idx += 1
+                
+                for idx, toke in enumerate(tokens) :
+                    fa.write(str(sent_idx) + ',' + toke.replace('\n', '') + ',' + tagged_sentence[idx] + '\n')
+                
+                #prediction.append(tagged_sentence)
+                
+    prediction.insert(8, ['O'] * len(gt[8]))
+    prediction.insert(15, ['O'] * len(gt[15]))  
+    
+    
+    from sklearn import metrics
+    
+    report = metrics.classification_report(gt, prediction)
+    with open(os.path.join(conf['model_dir'], 'results_reg.txt'), 'w') as fl:
+        fl.write(report)
+    
+    
+            
+    """
+    gt = []
+    prediction =
+    with open(os.path.join(conf['model_dir'], 'test_reg.csv'), 'r') as fa:
+    """
