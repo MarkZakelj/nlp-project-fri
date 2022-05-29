2b8cc9e4ee534cbd33bcff81ce254a3f6265babd
MarkZakelj <markzakelj@gmail.com>
2022-05-29 14:29:52 +0200

    evaluation

---
+++ evaluate.py
@@ -0,0 +1,136 @@
+import os
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+plt.rcParams['font.size'] = '15'
+
+import config_util
+
+def curve_precision(experiment, model):
+    df_true = pd.read_csv(os.path.join('data/experiments', experiment, 'test.csv'))
+    df_anno = pd.read_csv(os.path.join('data/experiments', experiment, model, 'annotation.csv'))
+    df_anno['Tag'] = df_anno['Tag'].apply(lambda x: x.lstrip('B-').lstrip('I-'))
+    entities = []
+    entity_id = -1
+    for sentence_id in df_anno['Sentence'].unique():
+        sentence = df_anno[df_anno['Sentence'] == sentence_id]
+        last_tag = None
+        for row in sentence.iterrows():
+            add_entity = False
+            row_id = row[0]
+            vals = row[1]
+            tag = vals['Tag']
+            if tag != 'O':
+                if tag != last_tag:
+                    entity_id += 1
+                add_entity = True
+                last_tag = tag
+            else:
+                last_tag = None
+
+            if add_entity:
+                entities.append((*vals, entity_id))
+            else:
+                entities.append((*vals, None))
+    df_true_new = pd.DataFrame.from_records(entities, columns=['Sentence', 'Word', 'Tag', 'Entity'])
+    df_true_new['New'] = df_true['Tag']
+    df_true_new['New'] = df_true_new['New'].apply(lambda x: x.lstrip('B-').lstrip('I-'))
+
+    tags = []
+    fig = plt.figure(figsize=(10, 8))
+    for tag in df_true_new['Tag'].unique():
+        if tag == 'O':
+            continue
+        tags.append(tag)
+        percents = df_true_new[df_true_new['Tag'] == tag].groupby('Entity').apply(
+            lambda x: sum(x['Tag'] == x['New']) / len(x)).to_numpy()
+        percents = np.sort(percents)
+        perc_unique = np.unique(percents)
+        x = [0]
+        y = [1]
+        for perc in perc_unique:
+            x.append(perc)
+            y.append(np.mean(percents >= perc))
+        plt.plot(x, y)
+    plt.xlabel('Threshold')
+    plt.ylabel('Precision')
+    plt.legend(tags, prop={'size': 12}, loc="lower left")
+
+    plt.grid()
+    plt.title('Precision curves')
+    plt.ylim(0, 1.1)
+    plt.show()
+
+
+    return entities
+
+
+def curve_recall(experiment, model):
+    df_true = pd.read_csv(os.path.join('data/experiments', experiment, 'test.csv'))
+    df_anno = pd.read_csv(os.path.join('data/experiments', experiment, model, 'annotation.csv'))
+
+    entities = []
+    entity_id = -1
+    for sentence_id in df_true['Sentence'].unique():
+        sentence = df_true[df_true['Sentence'] == sentence_id]
+
+        for row in sentence.iterrows():
+            new_entity = False
+            same_entity = False
+            row_id = row[0]
+            vals = row[1]
+            if vals['Tag'].startswith('B-'):
+                new_entity = True
+            elif vals['Tag'].startswith('I-'):
+                same_entity = True
+            if new_entity:
+                entity_id += 1
+            if new_entity or same_entity:
+                entities.append((*vals, entity_id))
+            else:
+                entities.append((*vals, None))
+    df_true_new = pd.DataFrame.from_records(entities, columns=['Sentence', 'Word', 'Tag', 'Entity'])
+    df_true_new['Anno'] = df_anno['Tag']
+    df_true_new['Anno'] = df_true_new['Anno'].apply(lambda x: x.lstrip('B-').lstrip('I-'))
+    df_true_new['Tag'] = df_true_new['Tag'].apply(lambda x: x.lstrip('B-').lstrip('I-'))
+    tags = []
+    fig = plt.figure(figsize=(10, 8))
+    for tag in df_true_new['Tag'].unique():
+        if tag == 'O':
+            continue
+        tags.append(tag)
+        percents = df_true_new[df_true_new['Tag'] == tag].groupby('Entity').apply(lambda x: sum(x['Tag'] == x['Anno']) / len(x)).to_numpy()
+        percents = np.sort(percents)
+        perc_unique = np.unique(percents)
+        x = [0]
+        y = [1]
+        for perc in perc_unique:
+            x.append(perc)
+            y.append(np.mean(percents >= perc))
+        plt.plot(x, y)
+    plt.xlabel('Threshold')
+    plt.ylabel('Recall')
+    plt.legend(tags)
+    plt.grid()
+    plt.title('Recall curves')
+    plt.ylim(0, 1.1)
+    plt.show()
+
+
+
+
+
+
+def main():
+    exps = config_util.list_experiments()
+    exp = exps[4]
+    mods = config_util.list_models(exp)
+    mod = mods[0]
+    print(exp, mod)
+    curve_precision(exp, mod)
+    curve_recall(exp, mod)
+
+
+if __name__ == '__main__':
+    main()
