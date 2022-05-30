99b152b438fea745e9ab25640e319f20382c3adc
unknown <ales.kert@gmail.com>
2022-05-30 14:43:18 +0200

    Fix GSW

--- relation_extraction_growing_window.py
+++ relation_extraction_growing_window.py
@@ -244,18 +244,15 @@ if __name__ == '__main__':
             
         gt = tokenize_sentence('test_reg.csv', arr_test_sent)
         prediction = tokenize_sentence('annotation_reg.csv', all_preds)
-        
-        fixed_prediction = []
+    
         for i, pred in enumerate(all_preds) :
             if len(pred) == 0 :
-                fixed_prediction.append(['O'] * len(gt[i]))
-            else :
-                fixed_prediction.append(prediction[i])
+                prediction.append(['O'] * len(gt[i]))
         
         print('Annotations saved to ' + os.path.join(conf['model_dir'], 'annotation_reg.csv'))
         
         
-        report = metrics.classification_report(sum(gt, []), sum(fixed_prediction, []))
+        report = metrics.classification_report(sum(gt, []), sum(prediction, []))
         with open(os.path.join(conf['model_dir'], 'results_reg.txt'), 'w') as fl:
             fl.write(report)
             print('Results saved to ' + os.path.join(conf['model_dir'], 'results_reg.txt'))
